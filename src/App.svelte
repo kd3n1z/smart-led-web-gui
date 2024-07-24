de1d284831729fbcbf835988d12ce17a1536cf0d
Denis Komarkov <denis.komarkov@gmail.com>
2024-07-24 19:12:12 +0300

    feat: loading current color on mount & fix api overload

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
-    function onChangeColor(color: string) {
-        const rgb = hexToRgb(color);
+    import { onMount } from 'svelte';
 
-        if (rgb != null) {
-            apiRequest(`set-solid-color ${rgb.r} ${rgb.g} ${rgb.b}`);
-        }
+    const apiBase = import.meta.env.DEV ? 'http://192.168.0.110/api?' : window.location.origin + '/api?';
+
+    let color: string;
+
+    onMount(async () => {
+        const rgb: number[] = (await apiRequest('get-solid-color'))[0].split(' ').map((e) => parseInt(e));
+
+        color = rgbToHex(rgb[0], rgb[1], rgb[2]);
+    });
+
+    function onChangeColor(value: string) {
+        color = value;
+
+        // on some browsers, the change event is called when a change occurs, not when the change is finished
+        // set a small delay and check if the color has changed to avoid overloading the API
+
+        setTimeout(() => {
+            if (color != value) {
+                return;
+            }
+
+            const rgb = hexToRgb(value);
+
+            if (rgb != null) {
+                apiRequest(`set-solid-color ${rgb.r} ${rgb.g} ${rgb.b}`);
+            }
+        }, 30);
     }
 
     function hexToRgb(hex: string) {
@@ -18,12 +41,23 @@
             : null;
     }
 
-    function apiRequest(...commandsArray: string[]) {
+    async function apiRequest(...commandsArray: string[]): Promise<string[]> {
         const commands = commandsArray.join(';');
 
         console.log('making api request: ' + commands);
 
-        fetch(window.location.origin + '/api?' + new URLSearchParams({ commands }));
+        const response = await (await fetch(apiBase + new URLSearchParams({ commands }))).text();
+
+        return response.split(';');
+    }
+
+    function componentToHex(c: number) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? '0' + hex : hex;
+    }
+
+    function rgbToHex(r: number, g: number, b: number) {
+        return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
     }
 </script>
 
@@ -31,6 +65,6 @@
     Smart LED Web GUI
 
     <div>
-        <input type="color" on:change={(e) => onChangeColor(e.currentTarget.value)} />
+        <input type="color" on:change={(e) => onChangeColor(e.currentTarget.value)} value={color} />
     </div>
 </main>
